fd725bd16a716e732fdac40e49744396c906a696
redoun-hossain <bd.myandme@gmail.com>
2026-07-19 22:43:26 +0600

    hihi

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    WEBHOOK_URL: 'https://arfinsami178.app.n8n.cloud/webhook-test/2be4a2d3-0f20-4a72-947d-98c04335b2ed',
+    WEBHOOK_URL: 'https://arfinsami178.app.n8n.cloud/webhook/2be4a2d3-0f20-4a72-947d-98c04335b2ed',
 };
